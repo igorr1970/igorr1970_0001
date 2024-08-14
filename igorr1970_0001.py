15f33cff5c9d8ca47aa35a2b42484a6120e80434
igorr1970 <igorr1970@mail.ru>
2024-08-14 20:51:27 +0500

    Add files via upload

---
+++ igorr1970_0001.py
@@ -0,0 +1,6 @@
+a=9  # 1st program
+b=0.5  # 1st program
+c=5  # 1st program
+d=(a**b)  # 1st program
+e=d*5  # 1st program
+print(e)  # 1st program
---
+++ igorr1970_0004.py
@@ -0,0 +1,6 @@
+a='123.456'  # 4st program
+b=float(a)  # 4st program
+c=(b)*10  # 4st program
+d=int(c)  # 4st program
+e=(d)%10  # 4st program
+print(e)  # 4st program
